309a45cb870391569d42bdde5285defa44dc9acc
DannyPomeranian <pnomarevdanny@gmail.com>
2026-01-15 19:50:23 +1100

    reformatted the file with pre-commit

--- robot.py
+++ robot.py
@@ -15,7 +15,7 @@ from components.vision import VisualLocalizer
 from ids import DioChannel, PwmChannel, RioSerialNumber
 from utilities.scalers import rescale_js
 
-
+#ignore this
 class MyRobot(magicbot.MagicRobot):
     # Components
     chassis: ChassisComponent
