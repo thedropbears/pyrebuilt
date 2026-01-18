c79a1aff596611ae5da828bcf116fd5f39204798
Lucien Morey <lucien@thedropbears.org.au>
2026-01-18 15:04:45 +1100

    make inversion config persist

--- components/climber.py
+++ components/climber.py
@@ -2,7 +2,7 @@ from magicbot import tunable, will_reset_to
 from rev import SparkMax, SparkMaxConfig
 
 from ids import SparkId
-from utilities.rev import configure_spark_ephemeral
+from utilities.rev import configure_spark_reset_and_persist
 
 
 class ClimberComponent:
@@ -15,7 +15,7 @@ class ClimberComponent:
         config = SparkMaxConfig()
         config.inverted(True)
         config.setIdleMode(SparkMaxConfig.IdleMode.kBrake)
-        configure_spark_ephemeral(self.motor, config)
+        configure_spark_reset_and_persist(self.motor, config)
 
     def deploy(self):
         self.setpoint = -self.climb_speed
