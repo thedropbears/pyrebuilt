46496d3422df3792ab2c9587e26ed29c5684afa4
SyntaxBreak <>
2026-01-19 17:00:22 +1100

    made the motor run in position control mode

--- components/climber.py
+++ components/climber.py
@@ -9,13 +9,21 @@ class ClimberComponent:
     setpoint = will_reset_to(0.0)
     climb_speed = tunable(0.2)
 
+    kP, kI, kD = 0, 0, 0
+    kMinOutput, kMaxOutput = 0, 0
+
     def __init__(self):
         # create motor with correct forward direction sparkmax controller
         self.motor = SparkMax(SparkId.CLIMBER, SparkMax.MotorType.kBrushless)
+        self.controller = self.motor.getClosedLoopController()
+
         config = SparkMaxConfig()
         config.inverted(True)
         config.setIdleMode(SparkMaxConfig.IdleMode.kBrake)
         configure_spark_reset_and_persist(self.motor, config)
+
+        config.closedLoop.pid(self.kP, self.kI, self.kD)
+        config.closedLoop.outputRange(self.kMinOutput, self.kMaxOutput)
 
     def deploy(self):
         self.setpoint = -self.climb_speed
@@ -24,4 +32,4 @@ class ClimberComponent:
         self.setpoint = self.climb_speed
 
     def execute(self):
-        self.motor.set(self.setpoint)
+        self.controller.setSetpoint(self.setpoint, SparkMax.ControlType.kPosition)
