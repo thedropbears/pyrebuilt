48e85bebc85722210b421146a4faed497e3a92b4
DannyPomeranian <pnomarevdanny@gmail.com>
2026-01-22 17:38:51 +1100

    Create the bare minimum skeleton for the ballistics component

---
+++ components/ballistics.py
@@ -0,0 +1,12 @@
+class BallisticsComponent:
+    def __init__(self):
+        pass
+
+    def calculate_required_shooter_angle(self):
+        pass
+
+    def calculate_required_hood_angle(self):
+        pass
+
+    def execute(self):
+        pass
