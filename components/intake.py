20df50db972950b4c79e7e21e6c8c1437b709b27
SyntaxBreak <>
2026-01-23 19:50:00 +1100

    Fixed prek

--- components/intake.py
+++ components/intake.py
@@ -21,7 +21,7 @@ class IntakeComponent:
         self.right_funnel_motor = TalonSRX(TalonId.RIGHT_FUNNEL)
 
         self.right_funnel_motor.setInverted(True)
-        
+
         motor_config = SparkMaxConfig()
         motor_config.setIdleMode(SparkMaxConfig.IdleMode.kCoast)
         configure_spark_ephemeral(self.motor, motor_config)
