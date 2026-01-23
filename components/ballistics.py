6f086bb9f8e6fcbedd22fa7dfd82052e75ddf8af
DannyPomeranian <pnomarevdanny@gmail.com>
2026-01-23 14:43:53 +1100

    change skeleton functions

--- components/ballistics.py
+++ components/ballistics.py
@@ -1,11 +1,16 @@
+from components.chassis import ChassisComponent
+
+
 class BallisticsComponent:
+    chassis: ChassisComponent
+
     def __init__(self):
         pass
 
-    def calculate_required_shooter_angle(self):
+    def distance_to_hub(self):
         pass
 
-    def calculate_required_hood_angle(self):
+    def shooter_angle_to_hub(self):
         pass
 
     def execute(self):
