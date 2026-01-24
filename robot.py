e9a39483c784062fb95a90d18842a0e371e6039b
SyntaxBreak <>
2026-01-24 15:08:55 +1100

    updated pids

--- robot.py
+++ robot.py
@@ -81,10 +81,10 @@ class MyRobot(magicbot.MagicRobot):
                 .with_k_a(0.082659),
                 steer_ratio=(14 / 50) * (10 / 60),
                 steer_gains=Slot0Configs()
-                .with_k_p(30.234)
+                .with_k_p(21.235)
                 .with_k_i(0)
-                .with_k_d(0.62183)
-                .with_k_s(0.1645),
+                .with_k_d(0.23781)
+                .with_k_s(0.092891),
                 reverse_drive=False,
             )
             # metres between centre of left and right wheels
