6af49f2dd4f1e1ff6268feb7da09326ba61a8806
DannyPomeranian <pnomarevdanny@gmail.com>
2026-01-24 20:25:34 +1100

    moved variables out of __init__

--- components/ballistics.py
+++ components/ballistics.py
@@ -8,21 +8,24 @@ class BallisticsComponent:
     chassis: ChassisComponent
 
     def __init__(self):
-        self.hub_pos = get_hub_pos(is_red())
-        self.chassis_pos = self.chassis.get_pose().translation()
-        self.chassis_rot = self.chassis.get_rotation()
+        pass
 
     def distance_to_hub(self):  # Distance from robot to hub
+        self.chassis_pos = self.chassis.get_pose().translation()
+
         return self.chassis_pos.distance(get_hub_pos(is_red()))
 
     def angle_to_hub(self):  # Angle from robot to hub
+        self.hub_pos = get_hub_pos(is_red())
+        self.chassis_pos = self.chassis.get_pose().translation()
+
         return atan2(
             self.hub_pos.y - self.chassis_pos.y, self.hub_pos.x - self.chassis_pos.x
         )
 
-    def required_shooter_angle_to_hub(
-        self,
-    ):  # Angle of shooter (relative to robot) to hub
+    def required_shooter_angle_to_hub(self):  # Angle of shooter to hub
+        self.chassis_rot = self.chassis.get_rotation()
+
         return self.angle_to_hub() - self.chassis_rot.radians()
 
     def execute(self):
