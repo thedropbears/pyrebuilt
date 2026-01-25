9eb8706685b400205dec7a03e236697626c743e8
DannyPomeranian <pnomarevdanny@gmail.com>
2026-01-25 22:14:40 +1100

    add turret angle calculations without accounting for chassis velocity

--- components/ballistics.py
+++ components/ballistics.py
@@ -1,3 +1,6 @@
+from math import atan2
+
+from magicbot import feedback
 from wpimath.geometry import Pose2d, Translation3d, Twist2d
 
 from components.shooter import ShooterComponent
@@ -10,11 +13,31 @@ class BallisticsComponent:
 
     # TODO setup sensible reset and tunable vars
 
+    chassis_pos = Pose2d().translation()
+    target_pos = Translation3d().toTranslation2d()
+    chassis_rot = Pose2d().rotation()
+
+    chassis_angle_to_target = float
+    shooter_angle_to_target = float
+    chassis_dist_to_target = float
+
     # TODO Define lookup table for use
 
     def __init__(self) -> None:
         # TODO Implement this
         pass
+
+    @feedback
+    def get_chassis_angle_to_target(self):
+        return self.chassis_angle_to_target
+
+    @feedback
+    def get_shooter_angle_to_target(self):
+        return self.shooter_angle_to_target
+
+    @feedback
+    def get_chassis_dist_to_target(self):
+        return self.chassis_dist_to_target
 
     def energise_flywheels(self) -> None:
         # TODO Implement this
@@ -28,15 +51,28 @@ class BallisticsComponent:
         current_pose: Pose2d,
         current_twist: Twist2d,
     ) -> None:
-        # TODO implement this
         # like components with hardware attached we dont want to perform the
         # calculation here. Just set the required vars and wait for execute.
-        pass
+
+        self.chassis_pos = current_pose.translation()
+        self.chassis_rot = current_pose.rotation()
+        self.target_pos = target_position.toTranslation2d()
+        self.chassis_dist_to_target = self.chassis_pos.distance(self.target_pos)
 
     def execute(self) -> None:
         # TODO Implement this
 
         # perform turret calculations
+        self.chassis_angle_to_target = atan2(
+            self.target_pos.y - self.chassis_pos.y,
+            self.target_pos.x - self.chassis_pos.x,
+        )
+
+        self.shooter_angle_to_target = (
+            self.chassis_angle_to_target - self.chassis_rot.radians()
+        )
+
+        # TODO account for chassis speeds when calculating the turret angle
 
         # dispatch turret command
 
