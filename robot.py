4e135e12993baa9d9706df12bc7955387fcbf3bd
Lucien Morey <lucien@thedropbears.org.au>
2026-01-31 09:36:46 +1100

    collapse driving variables, clean unused code and tweak limits

--- robot.py
+++ robot.py
@@ -34,16 +34,12 @@ class MyRobot(magicbot.MagicRobot):
     turret: TurretComponent
     ballistics: BallisticsComponent
     port_vision: VisualLocalizer
-    teleop_max_speed = tunable(3.5)  # m/s
-    teleop_max_spin_rate = tunable(3.5)  # m/s
-    test_max_speed = tunable(0.25)  # m/s
-    test_max_spin_rate = tunable(0.25)  # m/s
+
+    # Driving constraints
+    upper_max_speed = tunable(2.0)  # m/s
+    upper_max_spin_rate = tunable(2.0)  # m/s
     lower_max_speed = tunable(1.0)  # m/s
     lower_max_spin_rate = tunable(1.0)  # m/s
-    inclination_angle = tunable(0.0)
-    is_robot_oriented = tunable(False)
-
-    START_POS_TOLERANCE = 0.2
 
     def createObjects(self) -> None:
         self.event_loop = wpilib.event.EventLoop()
@@ -164,12 +160,12 @@ class MyRobot(magicbot.MagicRobot):
         self.chassis.set_coast_in_neutral(False)
 
     def teleopPeriodic(self) -> None:
-        max_speed = self.teleop_max_speed
-        max_spin_rate = self.teleop_max_spin_rate
+        max_speed = self.lower_max_speed
+        max_spin_rate = self.lower_max_spin_rate
 
         if self.gamepad.getRightBumperButton():
-            max_speed = self.lower_max_speed
-            max_spin_rate = self.lower_max_spin_rate
+            max_speed = self.upper_max_speed
+            max_spin_rate = self.upper_max_spin_rate
 
         drive_x = -rescale_js(self.gamepad.getLeftY(), 0.05, 1.5) * max_speed
         drive_y = -rescale_js(self.gamepad.getLeftX(), 0.05, 1.5) * max_speed
@@ -197,8 +193,8 @@ class MyRobot(magicbot.MagicRobot):
         allowed_to_drive = self.gamepad.getRightBumperButton()
         if allowed_to_drive:
             # Set max speed
-            max_speed = self.test_max_speed
-            max_spin_rate = self.test_max_spin_rate
+            max_speed = self.lower_max_speed
+            max_spin_rate = self.lower_max_spin_rate
 
             if self.gamepad.getXButton():
                 drive_x = 0.75 * max_speed
