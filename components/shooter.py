3fb5dbd01a123b6654362ac5b0127dff3b789759
Lucien Morey <lucien@thedropbears.org.au>
2026-02-08 16:08:43 +1100

    convert feedback to degrees

--- components/shooter.py
+++ components/shooter.py
@@ -73,16 +73,16 @@ class ShooterComponent:
 
         configure_spark_reset_and_persist(self.hood_motor, hood_motor_cfg)
 
-        self.target_hood_angle = self.get_hood_angle()
+        self.target_hood_angle = self.hood_encoder.getPosition()
 
     @feedback
-    def get_hood_angle(self) -> units.radians:
-        return self.hood_encoder.getPosition()
+    def hood_angle_degrees(self) -> units.degrees:
+        return math.degrees(self.hood_encoder.getPosition())
 
     @feedback
     def hood_is_at_setpoint(self) -> bool:
         return math.isclose(
-            self.get_hood_angle(),
+            self.hood_encoder.getPosition(),
             self.target_hood_angle,
             abs_tol=self.hood_error_tolerance,
         )
