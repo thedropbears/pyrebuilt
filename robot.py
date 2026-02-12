fc1af784b5983d853ac126b8167c3723b37131dc
DannyPomeranian <pnomarevdanny@gmail.com>
2026-02-12 20:10:30 +1100

    fix use of translation3d for ballistics in robotpy

--- robot.py
+++ robot.py
@@ -237,7 +237,7 @@ class MyRobot(magicbot.MagicRobot):
         if self.gamepad.getRightTriggerAxis() > 0.5 or self.gamepad.getXButton():
             if self.gamepad.getRightTriggerAxis() > 0.5:
                 self.ballistics.calculate_for(
-                    Translation3d(RED_HUB_POS),
+                    RED_HUB_POS,
                     self.chassis.get_pose(),
                     self.chassis.get_velocity().toTwist2d(0.02),
                 )
