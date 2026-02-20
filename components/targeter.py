9c7aa4f495137919494c605cdb573ebb9b62df94
DannyPomeranian <pnomarevdanny@gmail.com>
2026-02-20 18:02:20 +1100

    add calculations for target in neutral and enemy zone

--- components/targeter.py
+++ components/targeter.py
@@ -1,5 +1,6 @@
 from magicbot import feedback
-from wpimath.geometry import Translation2d
+from wpilib import Field2d
+from wpimath.geometry import Rotation2d, Translation2d
 
 from components.chassis import ChassisComponent
 from utilities.game import (
@@ -15,8 +16,10 @@ from utilities.game import (
 class Targeter:
     chassis: ChassisComponent
 
-    def __init__(self) -> None:
-        self.target = Translation2d()
+    target = Translation2d()
+
+    def __init__(self, field: Field2d) -> None:
+        self.target_pos_obj = field.getObject("target_pose")
 
     @feedback
     def get_target(self) -> Translation2d:
@@ -27,14 +30,58 @@ class Targeter:
         return alliance_hub_pos(is_red())
 
     @feedback
-    def get_optimal_target_from_neutral_zone(self) -> Translation2d:
-        # TODO Fill in the actual logic for this
-        return Translation2d(0, 0)
+    def get_optimal_target_from_neutral_zone(
+        self,
+    ) -> Translation2d:  # Code for neutral zone and enemy zone is the same, but this MIGHT change in future, so keep it as seperate functions
+        robot_pos = self.chassis.get_pose().translation()
+
+        anchor1_pos = alliance_shoot_anchor_pos(is_red())[0]
+        anchor2_pos = alliance_shoot_anchor_pos(is_red())[1]
+
+        shot_x = alliance_shoot_line(is_red())
+
+        shot1_y = robot_pos.y + (
+            (anchor1_pos.y - robot_pos.y) / (anchor1_pos.x - robot_pos.x)
+        ) * (shot_x - robot_pos.x)
+
+        shot2_y = robot_pos.y + (
+            (anchor2_pos.y - robot_pos.y) / (anchor2_pos.x - robot_pos.x)
+        ) * (shot_x - robot_pos.x)
+
+        target1 = Translation2d(shot_x, shot1_y)
+        target2 = Translation2d(shot_x, shot2_y)
+
+        if robot_pos.distance(target1) <= robot_pos.distance(target2):
+            return target1
+        else:
+            return target2
 
     @feedback
-    def get_optimal_target_from_enemy_zone(self) -> Translation2d:
-        # TODO Fill in the actual logic for this
-        return Translation2d(0, 0)
+    def get_optimal_target_from_enemy_zone(
+        self,
+    ) -> Translation2d:  # Code for neutral zone and enemy zone is the same, but this MIGHT change in future, so keep it as seperate functions
+        robot_pos = self.chassis.get_pose().translation()
+
+        anchor1_pos = alliance_shoot_anchor_pos(is_red())[0]
+        anchor2_pos = alliance_shoot_anchor_pos(is_red())[1]
+
+        shot_x = alliance_shoot_line(is_red())
+
+        shot1_y = robot_pos.y + (
+            (anchor1_pos.y - robot_pos.y) / (anchor1_pos.x - robot_pos.x)
+        ) * (shot_x - robot_pos.x)
+
+        shot2_y = robot_pos.y + (
+            (anchor2_pos.y - robot_pos.y) / (anchor2_pos.x - robot_pos.x)
+        ) * (shot_x - robot_pos.x)
+
+        target1 = Translation2d(shot_x, shot1_y)
+        target2 = Translation2d(shot_x, shot2_y)
+
+        if robot_pos.distance(target1) <= robot_pos.distance(target2):
+            return target1
+        else:
+            return target2
 
     def execute(self) -> None:
         current_pos = self.chassis.get_pose().translation()
@@ -47,3 +94,5 @@ class Targeter:
 
         else:
             self.target = self.get_optimal_target_from_enemy_zone()
+
+        self.target_pos_obj.setPose(self.target.x, self.target.y, Rotation2d())
