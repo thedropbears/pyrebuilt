c2022788c047a45adb051a616755510da9ac1adc
Lucien Morey <lucien@thedropbears.org.au>
2026-02-21 13:41:11 +1100

    add indexing behavioir, tune, fix vars, update coordinate system

--- components/climber.py
+++ components/climber.py
@@ -8,10 +8,10 @@ from phoenix6.configs import (
     SoftwareLimitSwitchConfigs,
     TalonFXConfiguration,
 )
+from phoenix6.controls import PositionVoltage, VoltageOut
 from phoenix6.hardware import TalonFX
-from phoenix6.signals import InvertedValue, NeutralModeValue
+from phoenix6.signals import GravityTypeValue, InvertedValue, NeutralModeValue
 from rev import LimitSwitchConfig, SparkMax, SparkMaxConfig
-from wpimath.units import degreesToRotations
 
 from ids import SparkId, TalonId
 from utilities.rev import configure_spark_reset_and_persist
@@ -24,14 +24,16 @@ class ClimberComponent:
     SHAFT_RADIUS = 0.00733  # m
     FUDGE_FACTOR = 0.773  # FOR THE LIFE OF ME I DONT KNOW WHY. MAYBE THE BRAKE STAGE ISNT REALLY 1:1
 
-    RETRACTED_POS = 0
-    EXTENDED_POS = 0.174316
+    RETRACTED_POS = 0.0  # m
+    EXTENDED_POS = 0.22  # m
 
-    target_pos = tunable(EXTENDED_POS)
+    target_pos = tunable(RETRACTED_POS)
 
-    ALLOWABLE_OVERSPOOL = degreesToRotations(5)
+    ALLOWABLE_OVERSPOOL = 0.02  # m
     MAX_RETRACTION_LIMIT = RETRACTED_POS - ALLOWABLE_OVERSPOOL
     MAX_EXTENSION_LIMIT = EXTENDED_POS + ALLOWABLE_OVERSPOOL
+
+    INDEX_SEARCH_VOLTAGE = -8.0
 
     def __init__(self):
         # create motor with correct forward direction sparkmax controller
@@ -40,22 +42,13 @@ class ClimberComponent:
             SparkId.CLIMBER_SENSOR, SparkMax.MotorType.kBrushless
         )
 
-        # TODO check if you can actually configure the forward and reverse limit switches
         self.retraction_limit_switch = self.climber_sensor.getForwardLimitSwitch()
         self.extension_limit_switch = self.climber_sensor.getReverseLimitSwitch()
 
         sensor_config = SparkMaxConfig()
         sensor_config.limitSwitch.forwardLimitSwitchType(
             LimitSwitchConfig.Type.kNormallyClosed
-        ).forwardLimitSwitchTriggerBehavior(
-            LimitSwitchConfig.Behavior.kStopMovingMotor
-        ).reverseLimitSwitchType(
-            LimitSwitchConfig.Type.kNormallyClosed
-        ).reverseLimitSwitchTriggerBehavior(
-            LimitSwitchConfig.Behavior.kStopMovingMotorAndSetPosition
-        ).reverseLimitSwitchPosition(
-            0
-        )
+        ).reverseLimitSwitchType(LimitSwitchConfig.Type.kNormallyClosed)
 
         configure_spark_reset_and_persist(self.climber_sensor, sensor_config)
 
@@ -64,7 +57,7 @@ class ClimberComponent:
             .with_motor_output(
                 MotorOutputConfigs()
                 .with_neutral_mode(NeutralModeValue.BRAKE)
-                .with_inverted(InvertedValue.COUNTER_CLOCKWISE_POSITIVE)
+                .with_inverted(InvertedValue.CLOCKWISE_POSITIVE)
             )
             .with_feedback(
                 FeedbackConfigs().with_sensor_to_mechanism_ratio(
@@ -87,32 +80,48 @@ class ClimberComponent:
                 .with_reverse_soft_limit_enable(True)
             )
             .with_slot0(
-                Slot0Configs()  # TODO tune these
-                .with_k_p(0.5)
+                Slot0Configs()
+                .with_k_p(594.12)
                 .with_k_i(0)
-                .with_k_d(0)
-                .with_k_s(0)
-                .with_k_v(0)
-                .with_k_a(0)
+                .with_k_d(230.65)
+                .with_k_s(0.0030436)
+                .with_k_v(72.04)
+                .with_k_a(1.11881)
+                .with_k_g(0.16926)
+                .with_gravity_type(GravityTypeValue.ELEVATOR_STATIC)
             )
         )
 
     def deploy(self):
-        if not self.at_extension_limit():
-            self.target_pos = self.EXTENDED_POS
+        self.target_pos = self.EXTENDED_POS
 
     def retract(self):
-        if not self.at_retraction_limit():
-            self.target_pos = self.RETRACTED_POS
+        self.target_pos = self.RETRACTED_POS
 
     def try_index(self) -> None:
-        if self.at_retraction_limit():
-            self.climber_motor.set_position(0)
-            self.is_climber_indexed = True
+
+        if self.at_retraction_limit() and not self.has_indexed:
+            self.climber_motor.set_position(self.RETRACTED_POS)
+            self.has_indexed = True
+            self.target_pos = self.RETRACTED_POS
 
     def execute(self):
         self.try_index()
-        self.climber_motor.set_position(self.target_pos)
+
+        if self.has_indexed:
+            self.climber_motor.set_control(
+                PositionVoltage(
+                    self.target_pos,
+                    limit_forward_motion=self.at_extension_limit(),
+                    limit_reverse_motion=self.at_retraction_limit(),
+                )
+            )
+        else:
+            self.climber_motor.set_control(
+                VoltageOut(
+                    ClimberComponent.INDEX_SEARCH_VOLTAGE, ignore_software_limits=True
+                )
+            )
 
     @feedback
     def at_extension_limit(self) -> bool:
