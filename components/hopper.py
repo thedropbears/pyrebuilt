3fd94e03dd974eec917633373b3d79e6cd6fd3c1
rafzip <github@itsraf.com>
2026-02-28 11:01:41 +1100

    FXSify

--- components/hopper.py
+++ components/hopper.py
@@ -1,6 +1,6 @@
 from magicbot import tunable, will_reset_to
 from phoenix6 import configs
-from phoenix6.hardware import TalonFX
+from phoenix6.hardware import TalonFX, TalonFXS
 from phoenix6.signals import InvertedValue, NeutralModeValue
 
 from ids import TalonId
@@ -24,9 +24,9 @@ class HopperComponent:
             configs.TalonFXConfiguration().with_motor_output(indexer_output_config)
         )
 
-        self.feeder_motor = TalonFX(TalonId.FEEDER)
+        self.feeder_motor = TalonFXS(TalonId.FEEDER)
 
-        feeder_motor_config = configs.TalonFXConfiguration()
+        feeder_motor_config = configs.TalonFXSConfiguration()
         feeder_motor_config.motor_output.with_inverted(
             InvertedValue.COUNTER_CLOCKWISE_POSITIVE
         ).with_neutral_mode(NeutralModeValue.COAST)
