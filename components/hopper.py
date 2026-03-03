af3841a6653f1603c1cfbc5436459abcd13955a9
SyntaxBreak <kiaanjw@gmail.com>
2026-03-03 18:17:23 +1100

    rename feeder to injector, unslave it from indexer

--- components/hopper.py
+++ components/hopper.py
@@ -1,8 +1,7 @@
 from magicbot import tunable, will_reset_to
-from phoenix6 import configs
-from phoenix6.controls import Follower
+from phoenix6.configs import MotorOutputConfigs, TalonFXConfiguration
 from phoenix6.hardware import TalonFX
-from phoenix6.signals import InvertedValue, MotorAlignmentValue, NeutralModeValue
+from phoenix6.signals import InvertedValue, NeutralModeValue
 
 from ids import TalonId
 
@@ -11,28 +10,37 @@ class HopperComponent:
     desired_indexer_dutycycle = tunable(0.5)
     target_indexer_dutycycle = will_reset_to(0.0)
 
+    desired_injector_dutycycle = tunable(0.5)
+    target_injector_dutycycle = will_reset_to(0.0)
+
     def __init__(self) -> None:
         self.indexer_motor = TalonFX(TalonId.INDEXER)
+        self.injector_motor = TalonFX(TalonId.INJECTOR)
 
         indexer_output_config = (
-            configs.MotorOutputConfigs()
+            MotorOutputConfigs()
             .with_inverted(InvertedValue.COUNTER_CLOCKWISE_POSITIVE)
             .with_neutral_mode(NeutralModeValue.COAST)
         )
+
         self.indexer_motor.configurator.apply(
-            configs.TalonFXConfiguration().with_motor_output(indexer_output_config)
+            TalonFXConfiguration().with_motor_output(indexer_output_config)
         )
 
-        self.feeder_motor = TalonFX(TalonId.FEEDER)
+        injector_output_config = (
+            MotorOutputConfigs()
+            .with_inverted(InvertedValue.CLOCKWISE_POSITIVE)
+            .with_neutral_mode(NeutralModeValue.COAST)
+        )
+
+        self.injector_motor.configurator.apply(
+            TalonFXConfiguration().with_motor_output(injector_output_config)
+        )
 
     def feed(self) -> None:
         self.target_indexer_dutycycle = self.desired_indexer_dutycycle
+        self.target_injector_dutycycle = self.target_injector_dutycycle
 
     def execute(self) -> None:
         self.indexer_motor.set(self.target_indexer_dutycycle)
-        self.feeder_motor.set_control(
-            Follower(
-                TalonId.INDEXER,
-                MotorAlignmentValue(MotorAlignmentValue.OPPOSED),
-            )
-        )
+        self.injector_motor.set(self.target_injector_dutycycle)
