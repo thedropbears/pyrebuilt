2c21b424c13a5b4204ac912127e205a3b4d2188f
DannyPomeranian <danny.ponomarev@thedropbears.org.au>
2026-03-07 10:43:30 +1100

    remove deleted path from auto_test

--- autonomous/auto_test.py
+++ autonomous/auto_test.py
@@ -4,19 +4,6 @@ from autonomous.auto_base import AutoBase
 from ids import RioSerialNumber
 
 
-class Spin3m(AutoBase):
-    MODE_NAME = "Spin while forward for 3m"
-    if wpilib.RobotController.getSerialNumber() == RioSerialNumber.TEST_BOT:
-        DISABLED = True
-
-    def __init__(self):
-        super().__init__(
-            [
-                "spin3m",
-            ]
-        )
-
-
 class Spin3mandreturn(AutoBase):
     MODE_NAME = "spin forward 3m then come back"
     if wpilib.RobotController.getSerialNumber() == RioSerialNumber.TEST_BOT:
