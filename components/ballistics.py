43660328030a7ea6d717ebabd4364b09e361dc11
Alexander-Mironenko <alexander.mironenko@thedropbears.org.au>
2026-03-08 12:01:40 +1100

    retract hood in trench

--- components/ballistics.py
+++ components/ballistics.py
@@ -10,6 +10,7 @@ from wpimath.geometry import Rotation2d, Translation2d
 from components.chassis import ChassisComponent
 from components.shooter import ShooterComponent
 from components.turret import TurretComponent
+from utilities.game import is_in_transition_zone
 
 # TODO: Tune lookup tables for use
 DISTANCE_LOOKUP_30 = np.array([1.0, 1.5, 2.0, 2.5, 3.0], dtype=float)
@@ -84,6 +85,9 @@ class BallisticsComponent:
             desired_hood_angle,
         )
 
+    def retract_in_trench(self) -> None:
+        pass
+
     def execute(self) -> None:
         current_pose = self.chassis.get_pose()
 
@@ -116,3 +120,6 @@ class BallisticsComponent:
 
         self.shooter.pitch_to(target_hood_angle)
         self.turret.slew_to(target_turret_bearing)
+        if is_in_transition_zone:
+            self.shooter.pitch_to(self.desired_hood_angle)
+            pass
