4af70c94ecb377e8490cdcba0b76499a81ef6bb6
SyntaxBreak <kiaanjw@gmail.com>
2026-03-09 20:15:36 +1100

    fix wrong type

--- robot.py
+++ robot.py
@@ -257,7 +257,7 @@ class MyRobot(magicbot.MagicRobot):
             else:
                 self.ballistics.force_solution(
                     self.test_flywheel_speed,
-                    Rotation2d.fromDegrees(self.test_turret_angle),
+                    math.radians(self.test_turret_angle),
                     math.radians(self.test_hood_angle),
                 )
 
