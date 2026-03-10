b19683ca78f81801dc7ca620c8f09aae70f45171
rafzip <github@itsraf.com>
2026-03-10 15:49:51 +1100

    simplfy stuff in leds

--- components/leds.py
+++ components/leds.py
@@ -1,9 +1,25 @@
-from phoenix6.controls.color_flow_animation import ColorFlowAnimation
+from phoenix6.controls import ColorFlowAnimation, RainbowAnimation, SolidColor
 from phoenix6.hardware.candle import CANdle
-from phoenix6.signals.rgbw_color import RGBWColor
+from phoenix6.signals import RGBWColor
 
 from ids import CandleId
 from utilities.game import is_alliance_hub_active
+
+"""
+Avaible Animations:
+
+SolidColor
+EmptyAnimation
+ColorFlowAnimation
+FireAnimation
+LarsonAnimation
+RainbowAnimation
+RgbFadeAnimation
+SingleFadeAnimation
+StrobeAnimation
+TwinkleAnimation
+TwinkleOffAnimation
+"""
 
 
 class LEDComponent:
@@ -12,36 +28,37 @@ class LEDComponent:
     blue = RGBWColor(0, 0, 255, 0)
     green = RGBWColor(0, 255, 0, 0)
     white = RGBWColor(255, 255, 255, 255)
-    BRIGHTNESS = 1.0
+    LED_START = 0
+    LED_END = 255
 
     def __init__(self):
         self.candle = CANdle(device_id=CandleId.LED)
-        self.desired_command = ColorFlowAnimation(
-            led_start_index=0,
-            led_end_index=7,
-            color=LEDComponent.red,
-            slot=0,
+        self.set_rainbow()
+
+    def set_leds(self, animation, color):
+        self.desired_command = animation(
+            led_start_index=self.LED_START,
+            led_end_index=self.LED_END,
+            color=color,
         )
 
-    def _set_flow_colour(self, color: RGBWColor):
-        self.desired_command = ColorFlowAnimation(
-            led_start_index=0,
-            led_end_index=7,
-            color=color,
-            slot=0,
+    def set_rainbow(self):
+        self.desired_command = RainbowAnimation(
+            led_start_index=self.LED_START,
+            led_end_index=self.LED_END,
         )
 
     def set_test_lights(self):
-        self._set_flow_colour(LEDComponent.yellow)
+        self.set_leds(ColorFlowAnimation, color=self.yellow)
 
     def set_disabled_lights(self):
-        self._set_flow_colour(LEDComponent.red)
+        self.set_leds(SolidColor, color=self.white)
 
     def set_teleop_lights(self):
         if is_alliance_hub_active():
-            self._set_flow_colour(LEDComponent.green)
+            self.set_leds(ColorFlowAnimation, color=self.green)
         else:
-            self._set_flow_colour(LEDComponent.white)
+            self.set_leds(ColorFlowAnimation, color=self.white)
 
     def execute(self):
         self.candle.set_control(self.desired_command)
