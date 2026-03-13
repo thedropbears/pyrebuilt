f7d8ddfaa27419938b4783701f526ad8a5f6dcf5
SyntaxBreak <kiaanjw@gmail.com>
2026-03-13 11:40:46 +1100

    use joystick instead of controller for codriver

--- robot.py
+++ robot.py
@@ -77,7 +77,7 @@ class MyRobot(magicbot.MagicRobot):
         meta_table.putString("rio_serial", wpilib.RobotController.getSerialNumber())
 
         self.gamepad = wpilib.XboxController(0)
-        self.codriver_gamepad = wpilib.XboxController(1)
+        self.codriver_joystick = wpilib.Joystick(1)
         self.left_trigger_reset = True
 
         self.field = wpilib.Field2d()
@@ -214,7 +214,7 @@ class MyRobot(magicbot.MagicRobot):
         if self.gamepad.getRightTriggerAxis() > 0.5:
             self.conductor.shoot()
 
-        if self.codriver_gamepad.getRightBumperButtonPressed():
+        if self.codriver_joystick.getTrigger():
             self.conductor.log_shot()
 
     def testInit(self) -> None:
