ba05bbd688aeb413a60f3aac0aae1789ce8718e9
Alexander-Mironenko <alexander.mironenko@thedropbears.org.au>
2026-04-26 15:40:34 +1000

    Merge branch 'ballistics-lookup' of https://github.com/thedropbears/pyrebuilt into ballistics-lookup
    Co-authored-by: Adhiraj-Bagla <adhiraj.bagla@thedropbears.org.au>

---
+++ components/intakealextest.py
@@ -0,0 +1,26 @@
+# import magicbot
+
+
+# class IntakeComponent:
+#     def __init__(self):
+#         self.target_rps = rps ting
+#         self.desired_rps = rps ting
+#         self.intakedefaultposition = the starting position of the intake
+#         self.intakedeployedposition = the deployed position of the intake
+#         self.currentintakeposition = the position of the intake
+#         self.motor = motor ting
+#         self.rightdeploymotor = motor ting
+#         self.leftdeploymotor = motor ting
+
+#     def reverse(self):
+#         inverse self.motor
+
+#     def deploy(self):
+#         run right deploy motor until self.currentintakeposition == self.intakedeployedposition
+
+#     def intake(self):
+#         run intake self.motor
+
+#     def execute(self):
+#         target_rps = desired_rps
+#         reset rps
