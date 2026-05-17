3a77502e60adb23b59475ff970592276d67520cd
SyntaxBreak <88933260+SyntaxBreak@users.noreply.github.com>
2026-05-17 10:42:55 +1000

    move heading controller into auto_base

--- autonomous/auto_base.py
+++ autonomous/auto_base.py
@@ -10,11 +10,13 @@ from wpimath.kinematics import ChassisSpeeds
 from components.chassis import ChassisComponent
 from utilities import game
 
+heading_controller = PIDController(1.0, 0.0, 0.0)
 x_controller = PIDController(2.0, 0.0, 0.0)
 y_controller = PIDController(2.0, 0.0, 0.0)
 
 wpilib.SmartDashboard.putData("Auto X PID", x_controller)
 wpilib.SmartDashboard.putData("Auto Y PID", y_controller)
+wpilib.SmartDashboard.putData("Auto Heading PID", heading_controller)
 
 
 class AutoBase(AutonomousStateMachine):
@@ -77,7 +79,6 @@ class AutoBase(AutonomousStateMachine):
     @state(first=True)
     def initialising(self) -> None:
         # Add any tasks that need doing first
-        # self.chassis.heading_controller.setPID(Kp=1.0, Ki=0.0, Kd=0.0) TODO reimplement this with holonomic drive controller
         self.next_state("tracking_trajectory")
 
     @state
@@ -120,10 +121,8 @@ class AutoBase(AutonomousStateMachine):
         speeds = ChassisSpeeds(
             sample.vx + x_controller.calculate(pose.X(), sample.x),
             sample.vy + y_controller.calculate(pose.Y(), sample.y),
-            sample.omega,
-            # + self.chassis.heading_controller.calculate(
-            #     pose.rotation().radians(), sample.heading
-            # ),
+            sample.omega
+            + heading_controller.calculate(pose.rotation().radians(), sample.heading),
         )
 
         # Apply the generated speeds
