a636dbf80a40c7c91c8d0bfaf040d37caa120b75
David Vo <auscompgeek@users.noreply.github.com>
2026-06-10 18:54:24 +1000

    Move TunerSwerveDrivetrain to new module

--- components/chassis.py
+++ components/chassis.py
@@ -15,7 +15,8 @@ from wpimath.kinematics import (
 )
 from wpimath.units import rotationsToRadians, seconds
 
-from swerves.comp import TunerSwerveDrivetrain, tuner_constants
+from swerves.comp import tuner_constants
+from swerves.drivetrain import TunerSwerveDrivetrain
 from utilities.game import is_red
 from utilities.position import TeamPoses
 
