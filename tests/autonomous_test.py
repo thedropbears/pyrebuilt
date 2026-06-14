9eccacbb8039e1c887d142c8e5a0d6cece7f0f4d
rafzip <git@raf.zip>
2026-06-14 15:19:27 +1000

    annotate stuff in autonnomous_test correctly

--- tests/autonomous_test.py
+++ tests/autonomous_test.py
@@ -1,7 +1,6 @@
-from typing import Any
-
 import hal
 import pytest
+from pyfrc.test_support.controller import TestController
 from robotpy_ext.autonomous.selector_tests import (  # type: ignore[import-untyped]
     test_all_autonomous as _test_all_autonomous,
 )
@@ -10,8 +9,8 @@ from wpilib.simulation import DriverStationSim
 
 @pytest.mark.slow_integration_test
 @pytest.mark.parametrize("alliance", ["Red", "Blue"])
-def test_all_autonomous(control: Any, alliance: str):
-    station = getattr(hal.AllianceStationID, f"k{alliance}1")
+def test_all_autonomous(control: TestController, alliance: str):
+    station: hal.AllianceStationID = getattr(hal.AllianceStationID, f"k{alliance}1")
     DriverStationSim.setAllianceStationId(station)
 
     _test_all_autonomous(control)
