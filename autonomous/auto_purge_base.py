a08f469b14be44f66c255a47786d53e97a00cd8b
SyntaxBreak <88933260+SyntaxBreak@users.noreply.github.com>
2026-06-28 08:12:20 +1000

    add shoot and retreat paths

---
+++ autonomous/auto_purge_base.py
@@ -0,0 +1,147 @@
+import math
+from typing import override
+
+import choreo
+import wpilib
+from choreo.trajectory import SwerveSample, SwerveTrajectory
+from magicbot import AutonomousStateMachine, state, timed_state
+from wpilib import RobotBase
+from wpimath.controller import PIDController
+from wpimath.geometry import Pose2d
+from wpimath.kinematics import ChassisSpeeds
+
+from components.chassis import ChassisComponent
+from controllers.conductor import Conductor
+from utilities import game
+
+heading_controller = PIDController(1.0, 0.0, 0.0)
+heading_controller.enableContinuousInput(-math.pi, math.pi)
+x_controller = PIDController(2.0, 0.0, 0.0)
+y_controller = PIDController(2.0, 0.0, 0.0)
+
+wpilib.SmartDashboard.putData("Auto X PID", x_controller)
+wpilib.SmartDashboard.putData("Auto Y PID", y_controller)
+wpilib.SmartDashboard.putData("Auto Heading PID", heading_controller)
+
+
+class AutoPurgeBase(AutonomousStateMachine):
+    field: wpilib.Field2d
+    chassis: ChassisComponent
+    conductor: Conductor
+
+    def __init__(self, trajectory_names: list[str]) -> None:
+        # We want to parameterise these by paths and potentially a sequence of events
+        super().__init__()
+
+        self.current_leg = -1
+        self.starting_pose = None
+        self.trajectories: list[SwerveTrajectory] = []
+        for trajectory_name in trajectory_names:
+            try:
+                self.trajectories.append(choreo.load_swerve_trajectory(trajectory_name))
+                if self.starting_pose is None:
+                    self.starting_pose = self.get_starting_pose()
+            except ValueError:
+                # If the trajectory is not found, ChoreoLib already prints to DriverStation
+                pass
+
+    def setup(self) -> None:
+        #  setup path tracking controllers
+        self.auto_sample_field_obj = self.field.getObject("auto_sample")
+
+        # init any other defaults
+        pass
+
+    @override
+    def on_enable(self) -> None:
+        # configure defaults for pose in sim
+
+        # Setup starting position in the simulator
+        starting_pose = self.get_starting_pose()
+        if RobotBase.isSimulation() and starting_pose is not None:
+            self.chassis.set_pose(starting_pose)
+        # Reset the counter for which leg we are executing
+        self.current_leg = -1
+
+        super().on_enable()
+
+    def get_starting_pose(self) -> Pose2d | None:
+        return self.trajectories[0].get_initial_pose(game.is_red())
+
+    def _get_full_path_poses(self) -> list[Pose2d]:
+        """Get a list of poses for the full path for display."""
+        return [
+            sample.get_pose()
+            for trajectory in self.trajectories
+            for sample in trajectory.get_samples()
+        ]
+
+    def display_trajectory(self) -> None:
+        self.field.getObject("trajectory").setPoses(self._get_full_path_poses())
+
+    @override
+    def on_disable(self) -> None:
+        super().on_disable()
+        self.field.getObject("trajectory").setPoses([])
+
+    @state(first=True)
+    def initialising(self) -> None:
+        # Add any tasks that need doing first
+        self.next_state("prepping")
+
+    @timed_state(duration=1, next_state="shooting")
+    def prepping(self) -> None:
+        self.conductor.deploy_only()
+
+    @timed_state(duration=10, next_state="tracking_trajectory")
+    def shooting(self) -> None:
+        self.conductor.shoot()
+
+    @state
+    def tracking_trajectory(self, initial_call: bool, state_tm: float) -> None:
+        if initial_call:
+            self.current_leg += 1
+
+            if self.current_leg == len(self.trajectories):
+                self.done()
+                return
+
+            trajectory = (
+                self.trajectories[self.current_leg].flipped()
+                if game.is_red()
+                else self.trajectories[self.current_leg]
+            )
+
+            trajectory_poses = trajectory.get_poses()
+            self.field.getObject("trajectory").setPoses(trajectory_poses)
+
+        final_pose = self.trajectories[self.current_leg].get_final_pose(game.is_red())
+        if final_pose is None:
+            self.done()
+            return
+
+        sample = self.trajectories[self.current_leg].sample_at(state_tm, game.is_red())
+        if sample is not None:
+            self.follow_trajectory(sample)
+            self.auto_sample_field_obj.setPose(sample.get_pose())
+
+        if state_tm > self.trajectories[self.current_leg].get_total_time():
+            self.next_state("tracking_trajectory")
+
+    def follow_trajectory(self, sample: SwerveSample):
+        # track path
+
+        pose = self.chassis.get_pose()
+
+        # Generate the next speeds for the robot
+        speeds = ChassisSpeeds(
+            sample.vx + x_controller.calculate(pose.X(), sample.x),
+            sample.vy + y_controller.calculate(pose.Y(), sample.y),
+            sample.omega
+            + heading_controller.calculate(pose.rotation().radians(), sample.heading),
+        )
+
+        # Apply the generated speeds
+        self.chassis.drive_field(speeds.vx, speeds.vy, speeds.omega)
+
+        self.conductor.shoot()
--- autonomous/shoot_only.py
+++ autonomous/shoot_only.py
@@ -1,5 +1,6 @@
 from magicbot import AutonomousStateMachine, timed_state
 
+from autonomous.auto_purge_base import AutoPurgeBase
 from controllers.conductor import Conductor
 
 
@@ -15,3 +16,21 @@ class ShootOnly(AutonomousStateMachine):
     @timed_state(duration=10)
     def caged_shooting(self) -> None:
         self.conductor.shoot()
+
+
+class ShootAndRetreatTower(AutoPurgeBase):
+    MODE_NAME = "Shoot and retreat from tower bump"
+
+    conductor: Conductor
+
+    def __init__(self):
+        super().__init__(["comp/retreat_from_tower_bump"])
+
+
+class ShootAndRetreatDepot(AutoPurgeBase):
+    MODE_NAME = "Shoot and retreat from depot bump"
+
+    conductor: Conductor
+
+    def __init__(self):
+        super().__init__(["comp/retreat_from_depot_bump"])
