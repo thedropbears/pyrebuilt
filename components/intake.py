0d940d19049f7f1674a114fb7b8262e2139a8a71
rafzip <git@raf.zip>
2026-06-27 12:39:21 +1000

    roller rps

--- components/intake.py
+++ components/intake.py
@@ -15,6 +15,7 @@ from phoenix6.controls import (
     MotionMagicVoltage,
     NeutralOut,
     PositionVoltage,
+    VelocityVoltage,
 )
 from phoenix6.hardware import CANcoder, TalonFX
 from phoenix6.signals import (
@@ -31,8 +32,8 @@ from ids import CancoderId, TalonId
 
 
 class IntakeComponent:
-    desired_roller_duty = tunable(0.6)
-    target_roller_duty = will_reset_to(0.0)
+    desired_roller_rps = tunable(38.0)
+    target_roller_rps = will_reset_to(0.0)
 
     RETRACTED_INTAKE_ANGLE: units.radians = radians(90.0)
     DEPLOYED_INTAKE_ANGLE: units.radians = radians(-18.0)
@@ -59,6 +60,15 @@ class IntakeComponent:
         self.deployer_motor = TalonFX(TalonId.INTAKE_DEPLOYER)
         self.deployer_encoder = CANcoder(CancoderId.INTAKE)
 
+        roller_slot_config = (
+                    Slot0Configs()
+                    .with_k_v(0.15879)
+                    .with_k_a(0.0039303)
+                    .with_k_s(0.31864)
+                    .with_k_p(0.036116)
+                    .with_k_d(0.0)
+                )
+
         roller_motor_output_config = (
             MotorOutputConfigs()
             .with_inverted(InvertedValue.CLOCKWISE_POSITIVE)
@@ -73,7 +83,10 @@ class IntakeComponent:
             TalonFXConfiguration()
             .with_motor_output(roller_motor_output_config)
             .with_feedback(roller_motor_feedback_config)
+            .with_slot0(roller_slot_config)
         )
+
+        
 
         # siq hand tuned gains
         deployer_deploy_config = (
@@ -152,10 +165,10 @@ class IntakeComponent:
         self.target_deployer_angle = self.DEPLOYED_INTAKE_ANGLE
 
     def backdrive(self) -> None:
-        self.target_roller_duty = -self.desired_roller_duty
+        self.target_roller_rps = -self.desired_roller_rps
 
     def drive(self) -> None:
-        self.target_roller_duty = self.desired_roller_duty
+        self.target_roller_rps = self.desired_roller_rps
 
     def execute(self) -> None:
         if self.should_use_holding_config():
@@ -167,10 +180,10 @@ class IntakeComponent:
                 MotionMagicVoltage(self.target_deployer_angle / tau)
             )
 
-        if self.target_roller_duty == 0.0:
+        if self.target_roller_rps == 0.0:
             self.roller_motor.set_control(NeutralOut())
         else:
-            self.roller_motor.set(self.target_roller_duty)
+            self.roller_motor.set_control(VelocityVoltage(self.target_roller_rps))
 
     def is_retracted(self) -> bool:
         return isclose(
