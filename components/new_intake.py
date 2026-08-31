46bdf4f26a3a92ebfcd3da7820638af4cd819f36
Adhiraj Bagla <adhiraj.bagla@thedropbears.org.au>
2026-08-31 20:47:05 +1000

    Added closed loop control (maybe)

--- components/new_intake.py
+++ components/new_intake.py
@@ -1,9 +1,9 @@
 from magicbot import feedback, will_reset_to
-from phoenix6.controls import VoltageOut
+from phoenix6.configs import Slot0Configs
+from phoenix6.controls import DutyCycleOut, PositionVoltage
 from phoenix6.hardware import TalonFX
 from wpilib import AnalogEncoder
 from wpimath import units
-from wpimath.controller import PIDController
 
 from ids import DioChannel, TalonId
 
@@ -11,19 +11,26 @@ from ids import DioChannel, TalonId
 class IntakeComponent:
     target_roller_rps = will_reset_to(units.turns_per_second(0))
     target_intake_angle = will_reset_to(units.degrees(0))
+    RETRACTED_INTAKE_ANGLE = units.degrees(0)
+    DEPLOYED_INTAKE_ANGLE = units.degrees(75)
+    DESIRED_ROLLER_VOLTAGE = DutyCycleOut(1)
 
     def __init__(self) -> None:
-        self.motor = TalonFX(TalonId.INTAKE_DEPLOYER)
-        self.retracted_intake_angle = units.degrees(0)
-        self.deployed_intake_angle = units.degrees(75)
-        self.pid = PIDController(1, 0, 1)
+        self.intake_deployer = TalonFX(TalonId.INTAKE_DEPLOYER)
+        self.intake_roller = TalonFX(TalonId.INTAKE_ROLLER)
         self.encoder = AnalogEncoder(DioChannel.INTAKE_DEPLOYER_ENCODER)
+        slot0_configs = Slot0Configs()
+        slot0_configs.with_k_p(1)
+        slot0_configs.with_k_i(0)
+        slot0_configs.with_k_d(1)
+        self.intake_roller.configurator.apply(slot0_configs)
+        self.request = PositionVoltage(0).with_slot(0)
 
     def retract(self):
-        self.target_intake_angle = self.retracted_intake_angle
+        self.target_intake_angle = self.RETRACTED_INTAKE_ANGLE
 
     def deploy(self):
-        self.target_intake_angle = self.deployed_intake_angle
+        self.target_intake_angle = self.DEPLOYED_INTAKE_ANGLE
 
     def intake(self):
         pass
@@ -33,6 +40,5 @@ class IntakeComponent:
         return self.encoder.get()
 
     def execute(self):
-        effort = self.pid.calculate(self.target_intake_angle)
-        voltage = VoltageOut(effort)
-        self.motor.set_control(voltage)
+        self.intake_deployer.set_control(self.request.with_position(10))
+        self.intake_roller.set_control(self.DESIRED_ROLLER_VOLTAGE)
